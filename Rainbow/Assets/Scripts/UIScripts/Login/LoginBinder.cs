528727826a4ec895f3070c5027ff908bc7e805b8
wofead <h614667387@qq.com>
2020-12-22 22:36:48 +0800

    fairygui

---
+++ Rainbow/Assets/Scripts/UIScripts/Login/LoginBinder.cs
@@ -0,0 +1,14 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+
+namespace Login
+{
+    public class LoginBinder
+    {
+        public static void BindAll()
+        {
+            UIObjectFactory.SetPackageItemExtension(UI_LoginView.URL, typeof(UI_LoginView));
+        }
+    }
+}
