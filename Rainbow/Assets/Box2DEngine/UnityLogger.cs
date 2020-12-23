53442878aeb8fe04eddf386d293c21dd3f169fac
wofead <h614667387@qq.com>
2020-12-24 07:52:39 +0800

    Box2D

---
+++ Rainbow/Assets/Box2DEngine/UnityLogger.cs
@@ -0,0 +1,14 @@
+﻿using Box2DSharp.Common;
+using UnityEngine;
+
+namespace Box2DSharp
+{
+    public class UnityLogger : IDumpLogger
+    {
+        /// <inheritdoc />
+        public void Log(string message)
+        {
+            Debug.Log(message);
+        }
+    }
+}
